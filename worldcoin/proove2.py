e65194929bea86453c983d6faa98eb9672f45475
Mehdi <mehdi@MacBook-Pro-de-Mehdi.local>
2023-09-24 09:09:42 -0400

    fix : prov2

--- worldcoin/proove2.py
+++ worldcoin/proove2.py
@@ -6,29 +6,22 @@ def proove2(data, validator_private_key):
     authenticated_date = int(time.time())
 
     timestamp = Web3.toHex(Web3.toBytes(authenticated_date))
-
     data_bytes = Web3.toBytes(hexstr=data)
     timestamp_bytes = Web3.toBytes(hexstr=timestamp)
-
     message = data_bytes + timestamp_bytes
-
-    # Create an Ethereum account from the private key
     validator_account = Account.privateKeyToAccount(validator_private_key)
-
-    # Sign the message
     signature = Web3.toHex(validator_account.signHash(Web3.keccak(message)))
-
     proof = data + timestamp + signature
 
     return proof
 
 if __name__ == "__main__":
     # Replace with your actual data and private key
-    data = "0x1234567890abcdef"
+    data = "Swapping_token1_for_token2"
     validator_private_key = os.getenv("VALIDATOR_PRIVATE_KEY")
 
     generated_proof = proove2(data, validator_private_key)
     if generated_proof:
-        print("Generated Proof:", generated_proof)
+        print("Prooving:", generated_proof)
     else:
-        print("Proof generation failed.")
+        print("Error.")
